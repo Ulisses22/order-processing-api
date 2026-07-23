089d0dd6378513e64ce39c6242810c5cc252f787
Ulisses22 <112980693+Ulisses22@users.noreply.github.com>
2026-07-23 20:10:35 +0200

    init

---
+++ src/main/java/dev/ulisses/highperformanceapi/HighPerformanceApiApplication.java
@@ -0,0 +1,13 @@
+package dev.ulisses.highperformanceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighPerformanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighPerformanceApiApplication.class, args);
+	}
+
+}
