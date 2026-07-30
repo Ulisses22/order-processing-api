ad5a1feef3c5aa558247e2d13eda6aa6b04aab97
Ulisses22 <112980693+Ulisses22@users.noreply.github.com>
2026-07-30 15:02:15 +0200

    fix(repository): correct repository queries

--- src/main/java/dev/ulisses/highperformanceapi/domain/repository/NotificationRepository.java
+++ src/main/java/dev/ulisses/highperformanceapi/domain/repository/NotificationRepository.java
@@ -1,6 +1,8 @@
 package dev.ulisses.highperformanceapi.domain.repository;
 
 import dev.ulisses.highperformanceapi.domain.entity.Notification;
+import dev.ulisses.highperformanceapi.domain.enums.NotificationStatus;
+import dev.ulisses.highperformanceapi.domain.enums.NotificationType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,6 +10,12 @@ import java.util.UUID;
 
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
 
+    List<Notification> findByCustomerId(UUID customerId);
+
     List<Notification> findByOrderId(UUID orderId);
+
+    List<Notification> findByStatus(NotificationStatus status);
+
+    List<Notification> findByType(NotificationType type);
 
 }
--- src/main/java/dev/ulisses/highperformanceapi/domain/repository/ProductRepository.java
+++ src/main/java/dev/ulisses/highperformanceapi/domain/repository/ProductRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface ProductRepository extends JpaRepository<Customer, UUID> {
+public interface ProductRepository extends JpaRepository<Product, UUID> {
 
     boolean existsBySku(String sku);
 
