6d2a2c14fbcd1b38bf3407d3c390ff81958f9178
Ulisses22 <112980693+Ulisses22@users.noreply.github.com>
2026-08-05 20:03:34 +0200

    test(customer): Inject dependencies for integration test

--- src/test/java/dev/ulisses/highperformanceapi/web/CustomerControllerIT.java
+++ src/test/java/dev/ulisses/highperformanceapi/web/CustomerControllerIT.java
@@ -1,4 +1,29 @@
 package dev.ulisses.highperformanceapi.web;
 
-public class CustomerControllerIT {
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.ulisses.highperformanceapi.domain.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+class CustomerControllerIT {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    
+
 }
