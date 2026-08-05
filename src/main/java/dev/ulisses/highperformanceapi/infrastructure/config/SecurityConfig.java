0639285667853023f76c207bb5245b529d659b4a
Ulisses22 <112980693+Ulisses22@users.noreply.github.com>
2026-08-05 21:08:45 +0200

    Update SecurityConfig.java

--- src/main/java/dev/ulisses/highperformanceapi/infrastructure/config/SecurityConfig.java
+++ src/main/java/dev/ulisses/highperformanceapi/infrastructure/config/SecurityConfig.java
@@ -1,16 +1,27 @@
 package dev.ulisses.highperformanceapi.infrastructure.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${APP_SECURITY_USERNAME}")
+    private String username;
+
+    @Value("${APP_SECURITY_PASSWORD}")
+    private String password;
 
     @Bean
     SecurityFilterChain security(HttpSecurity http) throws Exception{
@@ -24,5 +35,15 @@ public class SecurityConfig {
                     .anyRequest().authenticated()
             );
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.withUsername(username)
+                .password("{noop}" + password)
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 }
