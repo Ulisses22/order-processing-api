1bc6bc54958f04dd71232472c18d2ec7aefe2845
Ulisses22 <112980693+Ulisses22@users.noreply.github.com>
2026-08-24 00:23:38 +0200

    feat(k8s): complete core kubernetes setup

--- src/main/java/dev/ulisses/highperformanceapi/infrastructure/config/SecurityConfig.java
+++ src/main/java/dev/ulisses/highperformanceapi/infrastructure/config/SecurityConfig.java
@@ -53,6 +53,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/actuator/health",
+                                "/actuator/health/**",
                                 "/api/v1/auth/login",
                                 "/api/v1/auth/refresh",
                                 "/api/v1/auth/revoke",
